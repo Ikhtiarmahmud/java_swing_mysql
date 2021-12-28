abd89514c75602f721eaca39a75a5517a977a999
ikhtiaruddin <ikhtiar.uddin@bs-23.net>
2021-12-29 00:39:16 +0600

    Add insert, search, update and delete function

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,5 @@
+import net.proteanit.sql.DbUtils;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,7 +23,9 @@ public class Employee {
         frame.setVisible(true);
     }
 
-    private JTextField txtid;
+    private JTextField employeeId;
+    private JScrollPane table_1;
+    private JButton searchButton;
 
     Connection con;
     PreparedStatement pst;
@@ -45,12 +49,142 @@ public class Employee {
         }
     }
 
+    public void table_load()
+    {
+        try
+        {
+            pst = con.prepareStatement("select * from employee");
+            ResultSet rs = pst.executeQuery();
+            table1.setModel(DbUtils.resultSetToTableModel(rs));
+        }
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
     public Employee() {
         connect();
+        table_load();
         saveButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                String employeeName, employeeSalary, employeeMobile;
+
+                employeeName = name.getText();
+                employeeSalary = salary.getText();
+                employeeMobile = mobile.getText();
+
+                try {
+                    pst = con.prepareStatement("insert into employee(name,salary,mobile) values(?,?,?)");
+                    pst.setString(1, employeeName);
+                    pst.setString(2, employeeSalary);
+                    pst.setString(3, employeeMobile);
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Record Added!");
+                    table_load();
+                    name.setText("");
+                    salary.setText("");
+                    mobile.setText("");
+                    name.requestFocus();
+                }
+
+                catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+
+                    String empid = employeeId.getText();
+
+                    pst = con.prepareStatement("select name, salary, mobile from employee where id = ?");
+                    pst.setString(1, empid);
+                    ResultSet rs = pst.executeQuery();
+
+                    if(rs.next()==true)
+                    {
+                        String empname = rs.getString(1);
+                        String emsalary = rs.getString(2);
+                        String emmobile = rs.getString(3);
+
+                        name.setText(empname);
+                        salary.setText(emsalary);
+                        mobile.setText(emmobile);
+
+                    }
+                    else
+                    {
+                        name.setText("");
+                        salary.setText("");
+                        mobile.setText("");
+                        JOptionPane.showMessageDialog(null,"Invalid Employee No");
+                    }
+                }
+                catch (SQLException ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
+        updateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String empid, employeeName, employeeSalary, employeeMobile;
+                employeeName = name.getText();
+                employeeSalary = salary.getText();
+                employeeMobile = mobile.getText();
+                empid = employeeId.getText();
+
+                try {
+                    pst = con.prepareStatement("update employee set name = ?, salary = ?, mobile = ? where id = ?");
+                    pst.setString(1, employeeName);
+                    pst.setString(2, employeeSalary);
+                    pst.setString(3, employeeMobile);
+                    pst.setString(4, empid);
+
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Record Update!");
+                    table_load();
+                    name.setText("");
+                    salary.setText("");
+                    mobile.setText("");
+                    name.requestFocus();
+                } catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
+            }
+        });
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String empid;
+                empid = employeeId.getText();
+
+                try {
+                    pst = con.prepareStatement("delete from employee  where id = ?");
+
+                    pst.setString(1, empid);
+
+                    pst.executeUpdate();
+                    JOptionPane.showMessageDialog(null, "Record Delete Successfully!");
+                    table_load();
+                    name.setText("");
+                    salary.setText("");
+                    mobile.setText("");
+                    name.requestFocus();
+                }
+
+                catch (SQLException e1)
+                {
+                    e1.printStackTrace();
+                }
             }
         });
     }
